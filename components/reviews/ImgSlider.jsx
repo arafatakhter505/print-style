3c2ab87a2019ec2a0d4280e6de187401c082108f
arafatakhter505 <arafatakhterforhad@gmail.com>
2023-11-23 02:59:29 +0600

    slider added

--- components/reviews/ImgSlider.jsx
+++ components/reviews/ImgSlider.jsx
@@ -1,17 +1,57 @@
-import { Slider1 } from "@/public";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Slider1, Slider2, Slider3, Slider4 } from "@/public";
 import Image from "next/image";
 
 const ImgSlider = () => {
-  const breakPoints = [
-    { width: 1, itemsToShow: 1 },
-    { width: 550, itemsToShow: 2 },
-    { width: 768, itemsToShow: 3 },
-    { width: 1200, itemsToShow: 4 },
-  ];
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    autoPlay: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  const sliderItems = [Slider1, Slider2, Slider3, Slider4];
 
   return (
-    <div>
-      <h2>img</h2>
+    <div className="mb-10">
+      <Slider {...settings}>
+        {sliderItems.map((item) => (
+          <div>
+            <Image src={item} alt="product" />
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
